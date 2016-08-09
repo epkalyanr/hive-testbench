7137005deba1afcf29504d01e5e0974d402adf02
kraman88 <kraman88@gmail.com>
2016-08-08 19:41:58 -0700

    deleting old scripts

--- TPCHScripts/RunQueryLoop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..5}
-do
-
-sudo ./patdata.sh H@doop1234 ./tpch-queryexec_perquery.sh
-
-done
--- TPCHScripts/RunQuerySuite_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1..22}
-do	
-	./GetPatData.sh "Microsoft@1" ./tpch_queryexecute_all.sh $i tpch_query_$i
-done
